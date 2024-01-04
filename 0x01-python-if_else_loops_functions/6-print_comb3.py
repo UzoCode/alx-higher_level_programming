2f45919ad34638e94a3f8498dcc95e1196fe4aeb
Benedict Akosa <akosabenedict@gmail.com>
2024-01-04 06:58:13 -0800

    combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-for i in range(10):
-	for j in range(i + 1, 10):
-		if i == 0 and j == 1:
-			print(f"{i}{j}", end='')
-		else:
-			print(f", {i}{j}", end='')
-print()
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
