bc37c1b77ea3fed1b2dbd1d104d791aded465131
UzoCode <akosabenedict@gmail.com>
2024-02-06 09:23:39 +0000

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" OOP with python """
+
+class Square():
+    """This class represents a square"""
+
+    def __init__(self, size):
+        """ initialize class
+
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
